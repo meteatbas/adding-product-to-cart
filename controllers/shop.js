90a91a35a35ad384539b4c21e20dea2ea2c1ca2b
meteatbs <130202020@kocaeli.edu.tr>
2020-07-04 22:39:13 +0300

    update

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,6 @@
 const Product = require('../models/product');
-const Cart = require('../models/cart');
+
+
 
 exports.getProducts = (req, res, next) => {
   Product.findAll()
@@ -88,26 +89,34 @@ req.user.getCart().then(cart=>{
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;//>"productId" is the name we use in input in "product-detail.ejs"
   let fetchedCart;
+  let newQuantity=1;
   req.user.getCart()
-  .then(cart=>{
-    fetchedCart=cart;
-    return cart.getProducts({where:{id:prodId}});
-  }).then(products=>{
-    let product
-    if (products.length>0) {
-       product=products[0];
+  .then(cart => {
+    fetchedCart = cart;
+    return cart.getProducts({ where: { id: prodId } });
+  })
+  .then(products => {
+    let product;
+    if (products.length > 0) {
+      product = products[0];
     }
-    let newQuantity=1;
+
     if (product) {
-      //
+      const oldQuantity = product.cartItem.quantity;
+      newQuantity = oldQuantity + 1;
+      return product;
     }
-    return Product.findByPk(prodId).then(Product=>{
-      return fetchedCart.addProduct(product,{through:{quantity:newQuantity}});
-    }).catch(err=>console.log(err));
-  }).catch(err=>console.log(err));
-  
-  
-  
+    return Product.findByPk(prodId);
+  })
+  .then(product => {
+    return fetchedCart.addProduct(product, {
+      through: { quantity: newQuantity }
+    });
+  })
+  .then(() => {
+    res.redirect('/cart');
+  })
+  .catch(err => console.log(err));
   
   
   // Product.findById(prodId, product => {//we have to use product model(object) available in all the templates to use(access".product.price or product.desc") "productId"
@@ -118,25 +127,61 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
  const prodId=req.body.productId;  //we get productId by name in cart.ejs 
- Product.findById(prodId,product=>{
-  Cart.deleteProduct(prodId,product.price);
+ req.user.getCart().then(cart=>{
+  return cart.getProducts({where:{id:prodId}});
+ }).then(products=>{
+   const product=products[0];
+   return product.cartItem.destroy();
+ }).then(result=>{
   res.redirect('/cart');
- });
-
+ }).catch(err=>console.log(err))
 };
+
+exports.postOrder=(req,res,next)=>{
+  let fetchedCart;
+  req.user.getCart().then(cart=>{
+    fetchedCart=cart;
+    return cart.getProducts();
+  }).then(products=>{
+    return req.user.createOrder().then(order=>{
+      order.addProducts(products.map(product=>{
+        product.orderItem={quantity:product.cartItem.quantity};
+        return product;
+      }));
+    }).catch(err=>console.log(err));
+    
+    
+  }).then(result=>{
+    fetchedCart.setProducts(null);
+    
+  }).then(result=>{
+    res.redirect('/orders');
+  })
+  .catch(err=>console.log(err));
+}
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
-  });
-};
+  req.user.getOrders({include:['products']}).then(orders=>{//we use products because in app.js(55) we assosiciate an order to many product in modal
+  
+    
+    res.render('shop/orders', {
+      path: '/orders',
+      pageTitle: 'Your Orders',
+      orders:orders
+    });
+  }).catch(err=>console.log(err)
+  );};
 
 
-exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout'
-  });
-};
+
+
+
+
+
+// exports.getCheckout = (req, res, next) => {
+//   res.render('shop/checkout', {
+//     path: '/checkout',
+//     pageTitle: 'Checkout'
+//   });
+// };
 
--- routes/shop.js
+++ routes/shop.js
@@ -16,12 +16,14 @@ router.get('/cart', shopController.getCart);
 
 router.post('/cart-delete-item',shopController.postCartDeleteProduct);
 
+router.post('/create-order',shopController.postOrder);//form action in cart.ejs 
+
 router.post('/cart', shopController.postCart);
 
 router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
 router.get('/orders', shopController.getOrders);
 
-router.get('/checkout', shopController.getCheckout);
+// router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
